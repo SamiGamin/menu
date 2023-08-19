fab1f6ff896df9a96244a8a7a0625b4a7896c6ce
SamiGaming <104452588+SamiGamin@users.noreply.github.com>
2023-08-18 21:37:49 -0500

    menu

--- app/src/main/java/com/menu/MainActivity.java
+++ app/src/main/java/com/menu/MainActivity.java
@@ -105,7 +105,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void iniciar() {
-        Intent iniciar = new Intent(this, perfil.class);
+        Intent iniciar = new Intent(this, Inicio.class);
         startActivity(iniciar);
         finish();
         
